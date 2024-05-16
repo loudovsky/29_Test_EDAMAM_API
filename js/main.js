5bfa758a76de8d082444259a5ef158e6d64020a5
loudovsky <luileptitecossais@gmail.com>
2024-05-16 16:05:52 +0200

    commit in the afternoon

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ const wrapper = document.querySelector('.wrapper')
 const button = document.querySelector('#button')
 
 function generate() {
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${query.value}&app_id=edda156a&app_key=2e7e02af906bc64f498bda713fd1ff1a`)
+    fetch(`https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=${query.value}&app_id=edda156a&app_key=2e7e02af906bc64f498bda713fd1ff1a`)
       .then(response => response.json())
       .then(data => {
         console.log(data);
         wrapper.innerHTML= "";
         data.hits.forEach(function(oneImage){
-          wrapper.innerHTML += `<h3><a href="${oneImage.recipe.url}">${oneImage.recipe.label}</a></h3>`;
+          wrapper.innerHTML += `<h3><a href="${oneImage.recipe.url}" target="_blank">${oneImage.recipe.label}</a></h3>`;
           // Ajoute une classe parente qui contiendra la div image et la div recipe
           const parentDiv = document.createElement("div");
           parentDiv.className = "image-ingredients"; 
@@ -30,10 +30,13 @@ function generate() {
               ingredientElement.textContent = oneIngredient;
               ingredientsDiv.appendChild(ingredientElement);
             })
-
+            const instructionsElement = document.createElement("div")
+            instructionsElement.className = "instructions";
+            instructionsElement.textContent = oneImage.recipe.instructions;
             // Ajoute imageDiv & ingredientsDiv au wrapper
             parentDiv.appendChild(imageDiv);
             parentDiv.appendChild(ingredientsDiv);
+            parentDiv.appendChild(instructionsElement);
 
             // Ajoute la div parente au conteneur principal (wrapper)
             wrapper.appendChild(parentDiv);
